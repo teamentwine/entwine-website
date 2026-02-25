51deb2da7e08e744d347719ee614259ae45c5ada
daria-718 <dariag@projectentwine.org>
2026-02-24 21:20:53 -0500

    Volunteer + Waitlist details

--- src/app/waitlist/page.tsx
+++ src/app/waitlist/page.tsx
@@ -5,8 +5,8 @@ export default function Waitlist () {
         <section id="waitlist_page" className="flex flex-col">
                 {/* Call to action */}
                 <div className="flex flex-col items-center xxs:mx-5 xs:mx-8 sm:mx-15 2xl:mx-20 xxs:mt-10 sm:mt-20 xl:mt-30 xxs:space-y-5 md:space-y-10 2xl:space-y-20 ">
-                    <p className="font-body text-secondary-base xxs:text-base sm:text-2xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold">JOIN THE</p>
-                    <p className="font-body xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-bold">WAITLIST<span className="text-secondary-base">!</span></p>
+                    <p className="font-body text-secondary-base xxs:text-base sm:text-3xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-extrabold">JOIN THE</p>
+                    <p className="font-body xxs:text-2xl xs:text-3xl sm:text-4xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-black">WAITLIST<span className="text-secondary-base">!</span></p>
                     <p className="font-body xxs:text-xs sm:text-base lg:text-lg 2xl:text-3xl text-center">If you're a non-profit interested in our mission or want to hear more about the vision, please fill out our interest form or email us at <Link href="#"><span className="underline">info@entwine.org</span>.</Link> We'd love to hear any feedback or ideas we can. Thank you!</p>
                 </div>
                 {/* Form */}
@@ -23,7 +23,7 @@ export default function Waitlist () {
                     <input type="text" id="subject" placeholder="* Subject" className="form-input" required/>
                     <textarea type="textarea" rows="5" id="message" className="form-input" placeholder="* Message" required/>
                     {/* Submit button */}
-                    <button type="submit" className="font-body bg-secondary-base-3 xxs:w-1/2 sm:w-1/3 lg:w-1/5 xl:w-1/7 rounded-2xl py-2 self-center border-2 border-dark-border drop-shadow-secondary font-bold cursor-pointer mt-5 xxs:text-sm">SUBMIT</button>
+                    <button type="submit" className="font-body bg-secondary-base-3 xxs:w-1/5 sm:w-1/4 lg:w-1/3 xl:w-1/2 rounded-2xl py-2 self-center border-2 border-dark-border drop-shadow-secondary font-bold cursor-pointer mt-5 xxs:text-sm">SUBMIT</button>
                 </form>
         </section>
     )
