7f610b4cc38c25332880e3a7f045a1d81e702aa5
Victoria <piklad1996@gmail.com>
2026-04-09 17:07:48 -0500

    Platform page save point

--- src/app/platform/page.tsx
+++ src/app/platform/page.tsx
@@ -17,15 +17,15 @@ export default function Platform () {
                 <div className="flex flex-col xxs:w-50 xs:w-70 lg:w-90 xxs:h-60 xs:h-80 md:h-100 lg:h-120 bg-gray-500 xxs:float md:absolute xxs:mt-[-40] md:mt-[-50] lg:mt-[-40] xxs:mx-auto md:mx-0 md:ml-[7%] lg:ml-[5%] xl:ml-[3%] 2xl:ml-[-2%]">
                 </div>
                 {/* Paragraphs */}
-                <div className="xxs:space-y-5 xxs:mt-5 lg:my-15 md:ml-[50%] lg:ml-[45%] md:mr-10 indent-5">
+                <div className="xxs:space-y-5 xxs:mt-5 lg:my-15 md:ml-[50%] lg:ml-[45%] md:mr-10 ">
                     <p className="font-body"> To achieve our goals, we are building a digital platform that focuses on minimizing the effort to: </p>
-                    <ul className="list-disc marker:text-secondary-base leading-15 font-light">
+                    <ul className="list-disc marker:text-secondary-base leading-15 font-light pl-10">
                         <li>Make first contact</li>
                         <li>Coordinate actions</li>
                         <li>Establish boundaries with any organization</li>
                     </ul>
                     <p className="font-body">Based on these principles we've decided our platform needs to have a way to:</p>
-                    <ul className="list-disc marker:text-secondary-base leading-15 font-light">
+                    <ul className="list-disc marker:text-secondary-base leading-15 font-light pl-10">
                         <li>Recommend potential organizations you can collaborate with</li>
                         <li>Facilitate communication</li>
                         <li>Signal to the wider community of what types of collaborations you are looking for</li>
@@ -35,43 +35,67 @@ export default function Platform () {
             </div>
             {/* Our Journey / Timeline History */}
 
-            <div className="flex  max-w-6xl  md:mx-auto">
+            <div className="flex xxs:flex-col xxs:mx-5 md:flex-row  max-w-6xl  md:mx-auto">
                 {/* Heading and image */}
-                <div className="flex flex-col">
+                <div className="flex xxs:flex-col-reverse md:flex-col md:mr-10">
                     <div>
-                        <p>OUR</p>
-                        <p>JOURNEY</p>
+                        <p className="text-6xl xxs:text-xl text-primary-base font-bold xxs:mb-5 md:mb-10">OUR</p>
+                        <p className="text-8xl xxs:text-4xl text-secondary-base font-bold">JOURNEY</p>
                     </div>
-                    <Image src={climbingImage} className="w-250" alt="three people climbing mountainside"/>
-                    <a href="https://www.vecteezy.com/free-vector/conference" className="text-[0.5rem] xxs:self-center xs:self-end">Conference Vectors by Vecteezy</a>
-
-                </div>
+                    <div>
+                        <Image src={climbingImage} className="w-250 xxs:justify-center" alt="three people climbing mountainside"/>
+                        <a href="https://www.vecteezy.com/free-vector/conference" className="text-[0.5rem] xxs:self-right xs:self-end">Conference Vectors by Vecteezy</a>
+                        </div>
+                    </div>
                 {/* timeline */}
-                <div className="font-body relative">
+                <div className="font-body relative xxs:mb-10  xxs:mt-20 md:mt-40">
                     {/* line */}
-                    <div className="left-31 z-[-1] h-100 w-0.5 bg-gray-400 absolute"></div>
+                    <div className="benchmark-line bg-[#989898] z-[-1] absolute"></div>
 
                     {/* benchmark */}
-                    <div className="flex space-x-10 z-10">
-                        <p className="whitespace-nowrap">AUG 2025</p>
-                        <div className="flex-none w-5 h-5 bg-blue-500 rounded-full"></div>
+                    <div className="flex benchmark-container">
+                        <p className="benchmark-heading font-bold whitespace-nowrap">AUG 2025</p>
+                        {/* circle marker */}
+                        <div className="benchmark-marker flex-none   bg-secondary-base border-1 rounded-full align-end"></div>
+                        {/* heading and text */}
                         <div className="flex flex-col">
-                            <p>Benchmark Heading</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl ut orci ultricies, ut malesuada felis elementum. Curabitur condimentum scelerisque ligula a iaculis.</p>
+                            <p className="benchmark-heading whitespace-nowrap font-bold">Benchmark Heading</p>
+                            <p className="benchmark-body-text font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl ut orci ultricies, ut malesuada felis elementum. Curabitur condimentum scelerisque ligula a iaculis.</p>
                         </div>
                     </div>
-
-                      {/* benchmark */}
-                      <div className="flex space-x-10 z-10">
-                        <p className="whitespace-nowrap">AUG 2025</p>
-                        <div className="flex-none w-5 h-5 bg-blue-500 rounded-full"></div>
+                    {/* benchmark */}
+                    <div className="flex benchmark-container">
+                        <p className="benchmark-heading font-bold whitespace-nowrap">AUG 2025</p>
+                        {/* circle marker */}
+                        <div className="benchmark-marker flex-none   bg-primary-base border-1 rounded-full align-end"></div>
+                        {/* heading and text */}
                         <div className="flex flex-col">
-                            <p>Benchmark Heading</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl ut orci ultricies, ut malesuada felis elementum. Curabitur condimentum scelerisque ligula a iaculis.</p>
+                            <p className="benchmark-heading whitespace-nowrap font-bold">Benchmark Heading</p>
+                            <p className="benchmark-body-text font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl ut orci ultricies, ut malesuada felis elementum. Curabitur condimentum scelerisque ligula a iaculis.</p>
                         </div>
                     </div>
-
-
+                    {/* benchmark */}
+                    <div className="flex benchmark-container">
+                        <p className="benchmark-heading font-bold whitespace-nowrap">AUG 2025</p>
+                        {/* circle marker */}
+                        <div className="benchmark-marker flex-none   bg-secondary-base border-1 rounded-full align-end"></div>
+                        {/* heading and text */}
+                        <div className="flex flex-col">
+                            <p className="benchmark-heading whitespace-nowrap font-bold">Benchmark Heading</p>
+                            <p className="benchmark-body-text font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl ut orci ultricies, ut malesuada felis elementum. Curabitur condimentum scelerisque ligula a iaculis.</p>
+                        </div>
+                    </div>
+                    {/* benchmark */}
+                    <div className="flex benchmark-container">
+                        <p className="benchmark-heading font-bold whitespace-nowrap">AUG 2025</p>
+                        {/* circle marker */}
+                        <div className="benchmark-marker flex-none   bg-primary-base border-1 rounded-full align-end"></div>
+                        {/* heading and text */}
+                        <div className="flex flex-col">
+                            <p className="benchmark-heading whitespace-nowrap font-bold">Benchmark Heading</p>
+                            <p className="benchmark-body-text font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl ut orci ultricies, ut malesuada felis elementum. Curabitur condimentum scelerisque ligula a iaculis.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
